6991c7178b2f4e0f348574598e31e6c5f14e4e0d
mmdbzr <mohammad.bzr82@gmail.com>
2025-07-30 23:35:36 +0330

    subscription page init

--- payment_form/src/App.tsx
+++ payment_form/src/App.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PaymentForm from "./components/PaymentForm";
 import { loadStripe } from "@stripe/stripe-js";
+import SubscribePage from "./components/SubscribePage";
 
 const stripePromise = loadStripe(
   "pk_test_51RqJqXQp1ktC5CcAvYF25m0gbkzUQ3ADNy0nx2H8xKcu1vKl2lW60Y6QQcworsrzmfhSdTzRFcLB4c00XGdnhTXW00fepnK49S"
@@ -35,7 +36,7 @@ function App() {
           <Router>
             <Routes>
               <Route path="/" element={<PaymentForm />} />
-              {/* <Route path="/subscribe" element={<SubscribePage />} /> */}
+              <Route path="/subscribe" element={<SubscribePage />} />
             </Routes>
           </Router>
         </Elements>
